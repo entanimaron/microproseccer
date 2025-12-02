bdbb20c524c4bb100142a45ddb565ea886377c32
entaimaron <shin.sone.1616@keio.jp>
2025-12-02 20:50:52 +0900

    micromain-addheader

---
+++ micro.h
@@ -0,0 +1,14 @@
+struct OBJECT
+{
+	int x;
+	int y;
+	int vx;
+	int vy;
+	char img;
+	int  wid;
+	int hei;
+	int life;
+	int state;
+	int ptn;
+    int timer;
+};
--- micromain.c
+++ micromain.c
@@ -24,20 +24,7 @@ int startPowerUp = 0;
 static int product = 1;
 int input_len = 0;
 char input_str[16] = {0};
-int playGame();  //ゲームをする関数
-void initGame();  //ゲームを初期化する関数
-void initVariable();  //変数を初期化する関数
-void movePlayer();  //playerの動きを定義する関数
-void setBullet();  //弾をセットする関数
-void moveBullet();  //弾の動きを決める関数
-int setEnemy(int x, int y, int vx, int vy, char img, int life, int ptn, int wid, int hei);  //敵をセットする関数
-void moveEnemy();  //敵(今は弾)の動きを決める関数
-void setItem();  //アイテムをセットする感ん数
-void moveItem();  //アイテムの動きを決める関数、ゲットした時の動作も決める
-void drawImg(int x, int y, char img);  //imgを描画
-void hitCheck();  //当たり判定を確認する関数
-int myRand();  //rand関数
-int createNum();  //合成数生成
+
 void drawFormula();
 int  btn_check_0();
 int  btn_check_1();
@@ -51,21 +38,6 @@ void lcd_clear_vbuf();
 int key_pad_scan();
 void handle_key_input();
 void check_factor_solution();
-
-struct OBJECT
-{
-	int x;
-	int y;
-	int vx;
-	int vy;
-	char img;
-	int  wid;
-	int hei;
-	int life;
-	int state;
-	int ptn;
-    int timer;
-};
 
 struct OBJECT player;  //プレイヤー
 struct OBJECT bullet[BULLET_MAX];  //自分が打つ弾
